4c3d1f7f6f7581ec90c7029921bf3bfe757bb963
nvavinash <avinashmk42@gmail.com>
2024-09-08 23:53:51 +0530

    Backend Part 8: Stripe integration

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
-const session = require('express-session');
-const passport = require('passport');
-const cors = require('cors');
-const LocalStrategy = require('passport-local').Strategy;
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const { createProduct } = require('./controller/Product');
-const productRouters = require('./routes/Product');
-const categoriesRouters = require('./routes/Category');
-const brandsRouters = require('./routes/Brands');
-const userRouter = require('./routes/User');
-const authRouter = require('./routes/Auth');
-const cartRouter = require('./routes/Cart');
-const ordersRouter = require('./routes/Order');
-const { User } = require('./model/User');
-const { isAuth, sanitizeUser ,cookieExtractor} = require('./services/common');
-const cookieParser = require('cookie-parser');
+const session = require("express-session");
+const passport = require("passport");
+const cors = require("cors");
+const LocalStrategy = require("passport-local").Strategy;
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const { createProduct } = require("./controller/Product");
+const productRouters = require("./routes/Product");
+const categoriesRouters = require("./routes/Category");
+const brandsRouters = require("./routes/Brands");
+const userRouter = require("./routes/User");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Cart");
+const ordersRouter = require("./routes/Order");
+const { User } = require("./model/User");
+const { isAuth, sanitizeUser, cookieExtractor } = require("./services/common");
+const cookieParser = require("cookie-parser");
 
 server.use(cors());
 
-
-const SECRET_KEY = 'SECRET_KEY';
+const SECRET_KEY = "SECRET_KEY";
 // JWT options
 const opts = {};
 opts.jwtFromRequest = cookieExtractor;
@@ -33,47 +32,50 @@ opts.secretOrKey = SECRET_KEY; // TODO: should not be in code;
 
 server.use(
   session({
-    secret: 'keyboard cat',
+    secret: "keyboard cat",
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
   })
 );
 server.use(cookieParser());
-server.use(passport.authenticate('session'));
+server.use(passport.authenticate("session"));
+server.use(express.raw({ type: "application/json" }));
 server.use(express.json());
-server.use('/products',isAuth(),productRouters.router);
-server.use('/categories',isAuth(),categoriesRouters.router);
-server.use('/brands',isAuth(),brandsRouters.router);
-server.use('/users',isAuth(),userRouter.router);
-server.use('/auth',authRouter.router);
-server.use('/cart',isAuth(),cartRouter.router);
-server.use('/order',isAuth(),ordersRouter.router);
+server.use("/products", isAuth(), productRouters.router);
+server.use("/categories", isAuth(), categoriesRouters.router);
+server.use("/brands", isAuth(), brandsRouters.router);
+server.use("/users", isAuth(), userRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", isAuth(), cartRouter.router);
+server.use("/order", isAuth(), ordersRouter.router);
 
 //Passport Strategies
 passport.use(
-  'local',
-  new LocalStrategy(
-    {usernameField: "email"},
-    async function (email, password, done) {
+  "local",
+  new LocalStrategy({ usernameField: "email" }, async function (
+    email,
+    password,
+    done
+  ) {
     // by default passport uses username
     try {
       const user = await User.findOne({ email: email });
       console.log(email, password, user);
       if (!user) {
-        return done(null, false, { message: 'invalid credentials' }); // for safety
+        return done(null, false, { message: "invalid credentials" }); // for safety
       }
       crypto.pbkdf2(
         password,
         user.salt,
         310000,
         32,
-        'sha256',
+        "sha256",
         async function (err, hashedPassword) {
           if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
-            return done(null, false, { message: 'invalid credentials' });
+            return done(null, false, { message: "invalid credentials" });
           }
           const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
-          done(null, {id:user.id, role:user.role}); // this lines sends to serializer
+          done(null, { id: user.id, role: user.role }); // this lines sends to serializer
         }
       );
     } catch (err) {
@@ -84,11 +86,11 @@ passport.use(
 
 //jwt Strategy starts here
 passport.use(
-  'jwt',
+  "jwt",
   new JwtStrategy(opts, async function (jwt_payload, done) {
     console.log({ jwt_payload });
     try {
-      const user = await User.findById( jwt_payload.id);
+      const user = await User.findById(jwt_payload.id);
       if (user) {
         return done(null, sanitizeUser(user)); // this calls serializer
       } else {
@@ -102,7 +104,7 @@ passport.use(
 
 // this creates session variable req.user on being called from callbacks
 passport.serializeUser(function (user, cb) {
-  console.log('serialize', user);
+  console.log("serialize", user);
   process.nextTick(function () {
     return cb(null, { id: user.id, role: user.role });
   });
@@ -111,12 +113,81 @@ passport.serializeUser(function (user, cb) {
 // this changes session variable req.user when called from authorized request
 
 passport.deserializeUser(function (user, cb) {
-  console.log('de-serialize', user);
+  console.log("de-serialize", user);
   process.nextTick(function () {
     return cb(null, user);
   });
 });
 
+//payment Stripe
+
+const stripe = require("stripe")(
+  "sk_test_51Ohu3MSDNwTsA3NP5VN8wBALpjSE8URsm4gkTQg6y0bDjp4M1o5qWaLE0G4rz9atju9xrOXkPVfbdFHzbAas7RQc00M2B4WUxc"
+);
+
+// const calculateOrderAmount = (items) => {
+// Calculate the order total on the server to prevent
+// people from directly manipulating the amount on the client
+//   return 1400;
+// };
+
+server.post("/create-payment-intent", async (req, res) => {
+  const { totalAmount } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    // amount: calculateOrderAmount(items),
+    amount: totalAmount * 100, // for decimal
+    currency: "inr",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+    // [DEV]: For demo purposes only, you should avoid exposing the PaymentIntent ID in the client-side code.
+    dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
+  });
+});
+
+// Webhook
+
+// TODO: we will capture actual order after deploying out server live on public URL
+
+const endpointSecret = "whsec_0e1456a83b60b01b3133d4dbe06afa98f384c2837645c364ee0d5382f6fa3ca2";
+
+server.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntentSucceeded = event.data.object;
+      console.log({paymentIntentSucceeded})
+      // Then define and call a function to handle the event payment_intent.succeeded
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
+
+//Connetion to DB---------------------------------------------------------->
 const connectToDB = async () => {
   try {
     await mongoose.connect(
@@ -133,8 +204,8 @@ server.get("/", (req, res) => {
   res.json({ status: "success" });
 });
 
-connectToDB().then(()=>{
+connectToDB().then(() => {
   server.listen(8080, () => {
     console.log("yep Server Started ..");
   });
-})
+});
